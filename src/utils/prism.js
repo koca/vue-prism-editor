8c9cfe9d526bbb98a6172756a884934efd58968b
Mesut Koca <imesutkoca@gmail.com>
2018-08-27 03:19:24 +0300

    cleanup

--- src/utils/prism.js
+++ src/utils/prism.js
@@ -1,14 +1,10 @@
-// import loadLanguages from "prismjs/components/index";
 import prism from "prismjs";
 import escapeHtml from "escape-html";
-
-// loadLanguages(["markup", "css", "javascript"]);
 
 function wrap(code, lang) {
   if (lang === "text") {
     code = escapeHtml(code);
   }
-  // return `<pre v-pre class="language-${lang}"><code>${code}</code></pre>`;
   return `<code>${code}</code>`;
 }
 
@@ -30,13 +26,6 @@ export default (str, lang) => {
   if (lang === "py") {
     lang = "python";
   }
-  // if (!prism.languages[lang]) {
-  //   try {
-  //     loadLanguages([lang]);
-  //   } catch (e) {
-  //     console.log(`Syntax highlight for language "${lang}" is not supported.`);
-  //   }
-  // }
   if (prism.languages[lang]) {
     const code = prism.highlight(str, prism.languages[lang], lang);
     return wrap(code, rawLang);

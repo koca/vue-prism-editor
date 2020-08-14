df6174a9c216359a547c99c07fd7cc2a5966800a
Mesut Koca <imesutkoca@gmail.com>
2020-08-14 21:34:37 +0300

    fix: fix #86 and auto register prism-editor when using umd bundle

--- packages/vue-prism-editor/tsdx.config.js
+++ packages/vue-prism-editor/tsdx.config.js
@@ -9,7 +9,22 @@ module.exports = {
   /**
    * @param {import('rollup/dist/rollup').InputOptions} config
    */
-  rollup(config) {
+  rollup(config, options) {
+    if (options.format === 'umd') {
+      // https://github.com/koca/vue-prism-editor/issues/86
+      config.output.intro = 'var global = typeof self !== undefined ? self : this;';
+      // auto register prism editor component
+      config.output.outro = `let GlobalVue = null
+if (typeof window !== 'undefined') {
+  GlobalVue = window.Vue
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue
+}
+if (GlobalVue) {
+  GlobalVue.component("PrismEditor", PrismEditor)
+}`;
+    }
+
     config.plugins.push(
       postcss({
         plugins: [
